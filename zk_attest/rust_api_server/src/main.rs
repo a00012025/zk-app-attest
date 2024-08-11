645d7941aa442465c67dd69a27d8efde38cedf72
TheNonEconomist <thenoneconomist@gmail.com>
2024-08-11 11:38:25 -0400

    server nearly done, jsut need to find a way to properly extract journal entry

--- zk_attest/rust_api_server/src/main.rs
+++ zk_attest/rust_api_server/src/main.rs
@@ -1,43 +1,54 @@
 use actix_web::{web, App, HttpServer, Responder};
-// use serde::{Deserialize, Serialize};
+use alloy_primitives::{Address, U256};
 use app_attest_core::types::AppAttestationRequest;
-use risc0_zkvm::{ExecutorEnv, Prover};
-use guest_crate::METHOD_ID; //TODO: wadiz?
+use risc0_zkvm::{ExecutorEnv, default_prover};
+use serde::{Deserialize, Serialize};
+use methods::{ZK_ATTEST_GUEST_ELF, ZK_ATTEST_GUEST_ID}; //TODO: wadiz?
 
+#[derive(Serialize, Deserialize)]
+struct Output {
+    address: Address,
+    value: U256
+}
 
 async fn generate_proof_post(input: web::Json<AppAttestationRequest>) -> impl Responder {
     //Run the proof generation code in the guest code
 
-    // Set up the executor environment
     let env = ExecutorEnv::builder()
-        .add_input(&input)
+        .write(&input)
+        .unwrap()
         .build()
         .unwrap();
 
-    // Create a prover instance
-    let prover = Prover::new(&METHOD_ID, env).unwrap();
+    let prover = default_prover();
 
     // Run the guest code
-    let receipt = prover.run().unwrap();
+    let prove_info = prover.prove(env, ZK_ATTEST_GUEST_ELF).unwrap();
 
-    // Verify the receipt
-    receipt.verify(METHOD_ID).unwrap();
+    // extract the receipt.
+    let receipt = prove_info.receipt;
 
-    // Extract and process the journal
-    let journal = receipt.journal;
-    let result = process_journal(journal);
+    // Decode the journal to get the address and value
+    let journal_bytes = receipt.journal.bytes.clone();
+    let (address, value) = <(Address, U256)>::abi_decode(&journal_bytes, true).unwrap();
 
+    println!("Address: {:?}", address);
+    println!("Value: {}", value);
+
+    // The receipt was verified at the end of proving, but the below code is an
+    // example of how someone else could verify this receipt.
+    receipt.verify(ZK_ATTEST_GUEST_ID).unwrap();
+    
     // Return the result as a JSON response
-    web::Json(result)
+    web::Json(Output { address, value })
 }
-
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/trigger-guest", web::post().to(trigger_guest_code))
+            .route("/trigger-host", web::post().to(generate_proof_post))
     })
     .bind("127.0.0.1:8080")?
     .run()
